eadf737a76ff2500e4b2d8673f337059c2a6be06
Vanity <ktkyletran@gmail.com>
2021-10-18 18:30:36 -0700

    file setup for form and posts components

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,27 @@
-function App() {
+import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@mui/material';
+import Posts from './components/Posts';
+import Form from './components/Form';
+
+const App = () => {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <Container maxwidth="lg" className="App">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">A-Project</Typography>
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 }
 
--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
+import postRoutes from './routes/posts.js';
+
 dotenv.config();
 
 const app = express();
@@ -13,6 +15,8 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 app.use(cors());
+
+app.use('/posts', postRoutes);
 
 mongoose.connect(process.env.MONGO_URI, { 
   useNewUrlParser: true,
