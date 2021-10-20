6d8a351d23a57254b43f319f9720e9f04825891d
Vanity <ktkyletran@gmail.com>
2021-10-19 20:45:35 -0500

    redux install and setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Container, AppBar, Typography, Grow, Grid } from '@mui/material';
 import Posts from './components/Posts';
 import Form from './components/Form';
+import useStyles from './styles';
+import { getPosts } from './actions/posts'
 
 const App = () => {
+  const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
+
   return (
     <Container maxwidth="lg" className="App">
-      <AppBar position="static" color="inherit">
-        <Typography variant="h2" align="center">A-Project</Typography>
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">A-Project</Typography>
       </AppBar>
       <Grow in>
         <Container>
