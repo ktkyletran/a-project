ba5dbc31fa46108dc2259cdb6adefaa2fc08c6e2
Vanity <ktkyletran@gmail.com>
2021-10-22 21:33:18 -0500

    added post cards

--- client/src/components/Posts/index.js
+++ client/src/components/Posts/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import Post from './Post';
 import useStyles from './styles';
+import { Grid, CircularProgress } from '@mui/material';
 
 
 const Posts = () => {
@@ -10,11 +11,17 @@ const Posts = () => {
 
   console.log(posts)
   return (
-    <div>
-      <h1>Posts</h1>
-      <Post />
-      <Post />
-    </div>
+    !posts.length ? <CircularProgress /> : (
+      <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+        {
+          posts.map((post) => (
+            <Grid key={post._id} item xs={12} sm={6}>
+              <Post post={post} />
+            </Grid>
+          ))
+        }
+      </Grid>
+    )
   )
 }
 
