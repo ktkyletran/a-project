da08cfe52a53cc1f4e5eaa289ac0316fb742f813
Vanity <ktkyletran@gmail.com>
2021-10-31 16:47:46 -0500

    created user model

---
+++ server/models/user.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  id: { type: String },
+});
+
+const User = mongoose.model("User", userSchema)
+
+export default User;
