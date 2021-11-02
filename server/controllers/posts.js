9569af52bb4306686694db791b76f97a29a61c8a
Vanity <ktkyletran@gmail.com>
2021-11-01 21:53:33 -0500

    added like and dislike logic

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -50,10 +50,22 @@ export const deletePost = async (req, res) => {
 export const likePost = async (req, res) => {
   const { id } = req.params;
 
+  if (!req.userId) return res.json({ message: 'Unauthorized action' })
+
   if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that ID.');
 
   const post = await PostMessage.findById(id);
-  const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true });
+
+  const index = post.likes.findIndex((id) => id === String(req.userId));
+
+  if (index === -1) {
+    // like
+    post.likes.push(req.userId);
+  } else {
+    //dislike
+    post.likes.filter((id) => id !== String(req.userId))
+  }
+  const updatedPost = await PostMessage.findByIdAndUpdate(id, post, { new: true });
 
   res.json(updatedPost);
 }
