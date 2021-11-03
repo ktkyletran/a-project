8f3f0b772aadb78825cb91a473676538f7edbb91
Vanity <ktkyletran@gmail.com>
2021-11-03 18:43:34 -0500

    added axios interceptor to obtain token

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,6 +2,14 @@ import axios from 'axios';
 
 const API = axios.create({ baseURL: 'http://localhost:5000' });
 
+API.interceptors.request.use((req) => {
+  if (localStorage.getItem('profile')) {
+    req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`
+  };
+
+  return req;
+})
+
 export const fetchPosts = () => API.get('/posts');
 export const createPost = (postData) => API.post('/posts', postData);
 export const updatePost = (id, postData) => API.patch(`/posts/${id}`, postData);
