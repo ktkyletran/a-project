a809598feee615ff1351d962bdfbb7c241bba895
Vanity <ktkyletran@gmail.com>
2021-11-03 18:41:46 -0500

    spread postData and added name property to update and create functions in dispatch. also removed creator field from postData state

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -9,8 +9,8 @@ import { createPost, updatePost } from '../../actions/posts';
 const Form = ({ currentId, setCurrentId }) => {
   const classes = useStyles();
   const post = useSelector((state) => currentId ? state.posts.find((p) => p._id === currentId) : null);
+  const user = JSON.parse(localStorage.getItem('profile'));
   const [postData, setPostData] = useState({
-    creator: '',
     title: '',
     message: '',
     tags: '',
@@ -26,9 +26,9 @@ const Form = ({ currentId, setCurrentId }) => {
     e.preventDefault();
 
     if (currentId) {
-      dispatch(updatePost(currentId, postData))
+      dispatch(updatePost(currentId, { ...postData, name: user?.userData?.name }))
     } else {
-      dispatch(createPost(postData))
+      dispatch(createPost({ ...postData, name: user?.userData?.name }))
     }
     clear();
   }
@@ -36,7 +36,6 @@ const Form = ({ currentId, setCurrentId }) => {
   const clear = () => {
     setCurrentId(null);
     setPostData({
-      creator: '',
       title: '',
       message: '',
       tags: '',
@@ -44,11 +43,20 @@ const Form = ({ currentId, setCurrentId }) => {
     });
   };
 
+  if (!user?.userData.name) {
+    return (
+      <Paper className={classes.paper}>
+        <Typography variant="h6" align="center">
+          Please sign in to create a post.
+        </Typography>
+      </Paper>
+    )
+  };
+
   return (
     <Paper className={classes.paper}>
       <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
         <Typography variant="h6">{currentId ? 'Edit' : 'Create'} Memory</Typography>
-        <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })} /> 
         <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
         <TextField name="message" variant="outlined" label="Message" fullWidth value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })} />
         <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value.split(',') })} />
