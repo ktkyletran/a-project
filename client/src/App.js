d33b5f08b1e2f802853edfb778e16e5dfaeffaaf
Vanity <ktkyletran@gmail.com>
2021-11-04 21:26:12 -0500

    added new routes for posts and added redirect for posts and auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,23 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import { Container } from '@mui/material';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Auth from './components/Auth';
+import PostDetails from './components/PostDetails'
 
 const App = () => {
+  const user = JSON.parse(localStorage.getItem('profile'));
   return (
     <BrowserRouter>
-      <Container maxwidth="lg" className="App">
+      <Container maxwidth="xl" className="App">
         <Navbar />
         <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/auth" exact component={Auth} />
+          <Route path="/" exact component={() => <Redirect to="/posts" />} />
+          <Route path="/posts" exact component={Home} />
+          <Route path="/posts/search" exact component={Home} />
+          <Route path="/posts/:id" exact component={PostDetails} />
+          <Route path="/auth" exact component={() => (!user ? <Auth /> : <Redirect to="/posts"/>)} />
         </Switch>
       </Container>
     </BrowserRouter>
