5ba5f5858609e2a3c7a2546f199978cbef1cb16f
Vanity <ktkyletran@gmail.com>
2021-11-10 20:54:09 -0600

    minor fixes

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -17,7 +17,7 @@ export const getPosts = async (req, res) => {
   const { page } = req.query;
 
   try {
-    const LIMIT = 4;
+    const LIMIT = 6;
     const startIndex = (Number(page) - 1) * LIMIT; // get the starting index of every page
     const total = await PostMessage.countDocuments({});
     const postList = await PostMessage.find().sort({ _id: -1 }).limit(LIMIT).skip(startIndex);
